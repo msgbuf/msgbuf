3c46e51b664d7b06f8bb994c173b7c1ab5f7f8aa
Bernhard Haumacher <haui@haumacher.de>
2022-10-25 21:24:35 +0200

    Updated to Java 11, converted API to module.

---
+++ de.haumacher.msgbuf/src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Bernhard Haumacher et al. All Rights Reserved.
+ */
+/**
+ * <i>MsgBuf</i> API module.
+ */
+module de.haumacher.msgbuf {
+	exports de.haumacher.msgbuf.data;
+
+	exports de.haumacher.msgbuf.server.io;
+
+	exports de.haumacher.msgbuf.util;
+
+	exports de.haumacher.msgbuf.coder;
+
+	exports de.haumacher.msgbuf.binary;
+
+	exports de.haumacher.msgbuf.graph;
+
+	exports de.haumacher.msgbuf.observer;
+
+	exports de.haumacher.msgbuf.io;
+
+	exports de.haumacher.msgbuf.graph.cmd;
+
+	exports de.haumacher.msgbuf.json;
+}
