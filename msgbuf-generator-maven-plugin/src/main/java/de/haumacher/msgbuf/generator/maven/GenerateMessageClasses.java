78462d17a00f79ce89106a8c06bbd0470b93bf48
Bernhard Haumacher <haui@haumacher.de>
2022-10-28 20:41:42 +0200

    Added maven plugin for generating protocol code.

---
+++ msgbuf-generator-maven-plugin/src/main/java/de/haumacher/msgbuf/generator/maven/GenerateMessageClasses.java
@@ -0,0 +1,80 @@
+package de.haumacher.msgbuf.generator.maven;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import de.haumacher.msgbuf.generator.Generator;
+
+/**
+ * Goal that invokes the MsgBuf compiler.
+ */
+@Mojo(name = GenerateMessageClasses.NAME, defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class GenerateMessageClasses extends AbstractMojo {
+	
+	/**
+	 * Name of the target.
+	 */
+	public static final String NAME = "generate";
+	
+	/**
+	 * Directory where to generate source code to.
+	 */
+	@Parameter(defaultValue = "${project.build.sourceDirectory}", property = "outputDir", required = true)
+	private File _outputDirectory;
+	
+	/**
+	 * Input where to search for <code>*.proto</code> files.
+	 */
+	@Parameter(defaultValue = "${project.build.sourceDirectory}", property = "input", required = true)
+	private File _input;
+
+	@Override
+	public void execute() throws MojoExecutionException {
+		File outputDirectory = _outputDirectory;
+		if (!outputDirectory.exists()) {
+			outputDirectory.mkdirs();
+		}
+		
+		try {
+			List<String> files;
+			if (_input.isDirectory()) {
+				files = Files.walk(_input.toPath())
+					.filter(p -> p.toFile().isFile())
+					.filter(p -> p.getFileName().toString().endsWith(".proto"))
+					.map(p -> p.toAbsolutePath().toString())
+					.collect(Collectors.toList());
+			} else {
+				files = Collections.singletonList(_input.getAbsolutePath());
+			}
+			if (files.isEmpty()) {
+				getLog().info("No protocol files found, skipping.");
+				return;
+			}
+		
+			ArrayList<String> argList = new ArrayList<>(Arrays.asList(Generator.OUTPUT_DIR_ARG, _outputDirectory.getAbsolutePath()));
+			argList.addAll(files);
+			String[] args = argList.toArray(new String[0]);
+			Generator.main(args);
+		} catch (Throwable ex) {
+			throw new MojoExecutionException("Failed to invoke the generatore: " + ex.getMessage(), ex);
+		}
+	}
+
+	/** 
+	 * The directory where source files are generated to.
+	 */
+	public File getOutputDirectory() {
+		return _outputDirectory;
+	}
+}
---
+++ msgbuf-generator-maven-plugin/src/test/java/de/haumacher/msgbuf/generator/maven/TestGenerateMessageClasses.java
@@ -0,0 +1,42 @@
+package de.haumacher.msgbuf.generator.maven;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+
+import org.apache.maven.plugin.testing.MojoRule;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class TestGenerateMessageClasses {
+	@Rule
+	public MojoRule rule = new MojoRule() {
+		@Override
+		protected void before() throws Throwable {
+		}
+
+		@Override
+		protected void after() {
+		}
+	};
+
+	@Test
+	public void testSomething() throws Exception {
+		File pom = new File("target/test-classes/project-to-test/");
+		assertNotNull(pom);
+		assertTrue(pom.exists());
+
+		GenerateMessageClasses generate = 
+				(GenerateMessageClasses) rule.lookupConfiguredMojo(pom, GenerateMessageClasses.NAME);
+		assertNotNull(generate);
+		generate.execute();
+
+		File outputDirectory = (File) generate.getOutputDirectory();
+		assertNotNull(outputDirectory);
+		assertTrue(outputDirectory.exists());
+
+		File touch = new File(outputDirectory, "my/company/MyMessage.java");
+		assertTrue(touch.exists());
+	}
+
+}
