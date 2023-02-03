3e34e01e0a558b88312769e69f116a7baa65e334
Bernhard Haumacher <haui@haumacher.de>
2023-02-03 18:36:43 +0100

    Cleanup: More convenience methods in Field AST type.

--- de.haumacher.msgbuf.generator/src/main/java/de/haumacher/msgbuf/generator/FieldOperations.java
+++ de.haumacher.msgbuf.generator/src/main/java/de/haumacher/msgbuf/generator/FieldOperations.java
@@ -5,6 +5,7 @@ package de.haumacher.msgbuf.generator;
 
 import de.haumacher.msgbuf.generator.ast.Field;
 import de.haumacher.msgbuf.generator.ast.Option;
+import de.haumacher.msgbuf.generator.ast.StringOption;
 
 /**
  * Mix-in interface for {@link Field} providing operations.
@@ -21,7 +22,52 @@ public interface FieldOperations {
 
 	/** Whether this field cannot be set by the application */
 	default boolean isDerived() {
-		return getOptions().get("Reverse") != null || getOptions().get("Container") != null;
+		return isReverse() || isContainer();
+	}
+
+	/** 
+	 * Whether this is a derived reference pointing to the owner of the object this referenec belongs to.
+	 */
+	default boolean isContainer() {
+		return getOptions().get("Container") != null;
+	}
+
+
+	/** 
+	 * Whether this is a reverse reference.
+	 */
+	default boolean isReverse() {
+		return getReverseAnnotation() != null;
 	} 
+
+	/**
+	 * Whether this field is the reverse to the reference with the given name.
+	 */
+	default boolean isReverseOf(String name) {
+		return name.equals(getReverse());
+	}
+
+	/**
+	 * The name of the inverse reference of this one.
+	 * 
+	 * @return The name of the reverse reference to this one, or <code>null</code> if no such
+	 *         reference exists.
+	 */
+	default String getReverse() {
+		StringOption reverseOption = (StringOption) getReverseAnnotation();
+		if (reverseOption == null) {
+			return null;
+		}
+		return reverseOption.getValue();
+	}
+
+
+	/** 
+	 * The annotation marking this reference as "reverse".
+	 */
+	default Option getReverseAnnotation() {
+		return getOptions().get("Reverse");
+	}
+
 
 }
